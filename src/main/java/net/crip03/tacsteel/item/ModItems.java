8bd3c1dc010bfbaf73cff06664d8684c97e67996
Crip03 <154907758+Crip03@users.noreply.github.com>
2024-07-07 11:09:16 +0200

    com1

--- src/main/java/net/crip03/tacsteel/item/ModItems.java
+++ src/main/java/net/crip03/tacsteel/item/ModItems.java
@@ -4,6 +4,7 @@ import net.crip03.tacsteel.TacSteel;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -15,7 +16,7 @@ public class ModItems {
     public static final RegistryObject<Item> ARMOR_PLATE = ITEMS.register("armor_plate",
             () -> new ArmorItem(ModArmorMaterials.PLATE, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> HOLSTER = ITEMS.register("holster",
-            () -> new Item(new Item.Properties()));
+            () -> new ArmorItem(ModArmorMaterials.PLATE, ArmorItem.Type.LEGGINGS, new Item.Properties()));
 
 
 
